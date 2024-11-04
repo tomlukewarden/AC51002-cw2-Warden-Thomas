cded0b220488b4adbdfe4c7e453d47135dacc9aa
tomwarden <tomlukeprogramming@outlook.com>
2024-11-04 13:50:25 +0000

    edited classes so they are more linked

--- banking_app.py
+++ banking_app.py
@@ -1,34 +1,29 @@
 class CustomerDetails:
-    def __init__(self, name, account_number, account_type, address, phone, email, balance):
+    def __init__(self, name, account_number, address, phone, email):
         self.name = name
         self.account_number = account_number
-        self.account_type = account_type
         self.address = address
         self.phone = phone
         self.email = email
-        self.balance = balance
-        
+    
 
-class CurrentAccount:
-    def __init__(self, account_number, account_type, balance, statement):
-        self.account_number = account_number
+class CurrentAccount(CustomerDetails):
+    def __init__(self, name, account_number, address, phone, email, account_type, balance, statement):
+        super().__init__(name, account_number, address, phone, email)
         self.account_type = account_type
         self.balance = balance
         self.statement = statement
         
 
 class SavingAccount(CurrentAccount):
-    def __init__(self, account_number, account_type, balance, statement, interest_rate):
-        super().__init__(account_number, account_type, balance, statement)
+    def __init__(self, name, account_number, address, phone, email, account_type, balance, statement, interest_rate):
+        super().__init__(name, account_number, address, phone, email, account_type, balance, statement)
         self.interest_rate = interest_rate
         
 
-class MortgageAccount:
-    def __init__(self, account_number, account_type, balance, statement, interest_rate, monthly_repayment, linked_account):
-        self.account_number = account_number
-        self.account_type = account_type
-        self.balance = balance
-        self.statement = statement
+class MortgageAccount(CurrentAccount):
+    def __init__(self, name, account_number, address, phone, email, account_type, balance, statement, interest_rate, monthly_repayment, linked_account_number):
+        super().__init__(name, account_number, address, phone, email, account_type, balance, statement)
         self.interest_rate = interest_rate
         self.monthly_repayment = monthly_repayment
-        self.linked_account = linked_account
+        self.linked_account_number = linked_account_number
